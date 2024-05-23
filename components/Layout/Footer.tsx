60493cee2918de2566853b6ff9494f0d8a2ff6ba
jobotics <jo_botics@proton.me>
2024-05-23 13:43:58 +0100

    fix/Fix typecheck issue

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -5,7 +5,12 @@ const Footer = () => {
     <footer className="mx-auto my-10">
       <div className="flex justify-center items-center text-sm text-secondary gap-1.5">
         <span>Powered by</span>
-        <Image src="/static/icons/Aurora.svg" width={20} height={20} />
+        <Image
+          src="/static/icons/Aurora.svg"
+          width={20}
+          height={20}
+          alt="Aurora logo"
+        />
         <span>Aurora</span>
       </div>
     </footer>
