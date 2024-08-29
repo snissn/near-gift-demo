636c52b731fa4ff70564ef1682777e7b6fc9a328
jobotics <jo_botics@proton.me>
2024-08-29 19:24:54 +0100

    fix: create intent loop on failure tx

--- src/app/swap/SwapForm/utils/isWalletConnected.ts
+++ src/app/swap/SwapForm/utils/isWalletConnected.ts
@@ -5,20 +5,21 @@ import {
   CONNECTOR_ETH_BASE,
 } from "@src/constants/contracts"
 
-export default function isWalletConnected(defuse_asset_id: string): boolean {
+export default function isWalletConnected(defuse_asset_id: string): string {
   const to = parseDefuseAsset(defuse_asset_id)
   const toNetworkId = `${to?.blockchain}:${to?.network}` as MapsEnum
+  const noAccountId = ""
 
   switch (toNetworkId) {
     case MapsEnum.ETH_BASE:
       const getEthBaseFromLocal = localStorage.getItem(CONNECTOR_ETH_BASE)
-      if (!getEthBaseFromLocal) return false
-      return true
+      if (!getEthBaseFromLocal) return noAccountId
+      return getEthBaseFromLocal
     case MapsEnum.BTC_MAINNET:
       const getBtcMainnetFromLocal = localStorage.getItem(CONNECTOR_BTC_MAINNET)
-      if (!getBtcMainnetFromLocal) return false
-      return true
+      if (!getBtcMainnetFromLocal) return noAccountId
+      return getBtcMainnetFromLocal
     default:
-      return false
+      return noAccountId
   }
 }
