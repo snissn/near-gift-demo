ddf1f6b3d0642cdeee3759509d4a67004d628415
jobotics <jo_botics@proton.me>
2024-10-17 10:04:12 +0100

    chore: enable deposit page
    - Update routes
    - Use DepositWidget from SDK
    - CSS enhancement, defuse-sdk styles should override global

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -1,74 +1,35 @@
 "use client"
 
-import Image from "next/image"
+import type { Transaction } from "@near-wallet-selector/core"
 import React from "react"
-import { type FieldValues, useForm } from "react-hook-form"
 
-import Accordion from "@src/components/Accordion"
-import Button from "@src/components/Button/Button"
-import ButtonSwitch from "@src/components/Button/ButtonSwitch"
-import Form from "@src/components/Form"
-import FieldComboInput from "@src/components/Form/FieldComboInput"
+import { DepositWidget } from "@defuse-protocol/defuse-sdk"
 import Paper from "@src/components/Paper"
-import type { NetworkToken } from "@src/types/interfaces"
-
-type FormValues = {
-  tokenIn: string
-  tokenOut: string
-}
+import { useWalletSelector } from "@src/providers/WalletSelectorProvider"
 
 export default function Deposit() {
-  const { handleSubmit, register } = useForm<FormValues>()
-
-  const onSubmit = (values: FieldValues) => {
-    console.log(values, "form submit")
-  }
-  const handleSwitch = () => {
-    console.log("form switch")
-  }
-  const handleSetMax = () => {
-    console.log("form set max")
-  }
+  const { accountId, selector } = useWalletSelector()
   return (
     <Paper title="Deposit">
-      <Form<FormValues>
-        handleSubmit={handleSubmit(onSubmit)}
-        register={register}
-      >
-        <FieldComboInput<FormValues>
-          fieldName="tokenIn"
-          label="You pay"
-          selected={{ name: "USD" } as NetworkToken}
-        />
-        <ButtonSwitch onClick={handleSwitch} />
-        <FieldComboInput<FormValues>
-          fieldName="tokenOut"
-          label="You receive"
-          selected={{ name: "AURORA" } as NetworkToken}
-        />
-        <Accordion
-          leftHeaderElement={
-            <>
-              <span className="text-sm font-medium">0.184 ETH = €2,681.60</span>
-              <span className="text-sm text-gray-700">($0.361)</span>
-            </>
-          }
-          rightHeaderElement={
-            <>
-              <Image
-                src="/static/icons/fire.svg"
-                width={12}
-                height={16}
-                alt="caret-down"
-              />
-              <span className="text-sm font-medium text-gray-700">$5.56</span>
-            </>
-          }
-        />
-        <Button type="submit" size="lg" fullWidth disabled>
-          Coming soon
-        </Button>
-      </Form>
+      <DepositWidget
+        accountId={accountId || ""}
+        signAndSendTransactionsNear={async (transactions) => {
+          assert(selector, "Wallet selector is not found")
+          const wallet = await selector.wallet()
+          const transactionResult = await wallet.signAndSendTransactions({
+            transactions,
+          })
+          return transactionResult && transactionResult.length > 0
+            ? transactionResult[0]
+            : ""
+        }}
+      />
     </Paper>
   )
+}
+
+function assert(condition: unknown, msg?: string): asserts condition {
+  if (!condition) {
+    throw new Error(msg)
+  }
 }
