823d7b4e4107d758477d583b94d1ae3008652c8b
jobotics <jo_botics@proton.me>
2024-10-24 11:17:05 +0100

    chore: extend token list interface and add small props updates

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -4,22 +4,18 @@ import React from "react"
 
 import { DepositWidget } from "@defuse-protocol/defuse-sdk"
 import Paper from "@src/components/Paper"
-import {
-  SendTransactionType,
-  SignInType,
-  useConnectWallet,
-} from "@src/hooks/useConnectWallet"
-import { useWalletSelector } from "@src/providers/WalletSelectorProvider"
+import { LIST_TOKENS } from "@src/constants/tokens"
+import { SignInType, useConnectWallet } from "@src/hooks/useConnectWallet"
 
 export default function Deposit() {
   const { state, sendTransaction } = useConnectWallet()
-  const { selector } = useWalletSelector()
+
   return (
     <Paper title="Deposit">
       <DepositWidget
+        tokenList={LIST_TOKENS}
         accountId={state.address || ""}
-        signAndSendTransactionsNear={async (transactions) => {
-          assert(selector, "Wallet selector is not found")
+        sendTransaction={async (transactions) => {
           const transactionResult = await sendTransaction({
             id: SignInType.NearWalletSelector,
             transactions,
@@ -31,10 +27,4 @@ export default function Deposit() {
       />
     </Paper>
   )
-}
-
-function assert(condition: unknown, msg?: string): asserts condition {
-  if (!condition) {
-    throw new Error(msg)
-  }
 }
