5357ca8b1841d2e955b08cd9b6408388cda7740d
jobotics <jo_botics@proton.me>
2024-10-25 10:54:02 +0100

    chore: add deposit success and failure notification

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -6,9 +6,12 @@ import { DepositWidget } from "@defuse-protocol/defuse-sdk"
 import Paper from "@src/components/Paper"
 import { LIST_TOKENS } from "@src/constants/tokens"
 import { SignInType, useConnectWallet } from "@src/hooks/useConnectWallet"
+import { useNotificationStore } from "@src/providers/NotificationProvider"
+import { NotificationType } from "@src/stores/notificationStore"
 
 export default function Deposit() {
   const { state, sendTransaction } = useConnectWallet()
+  const setNotification = useNotificationStore((state) => state.setNotification)
 
   return (
     <Paper title="Deposit">
@@ -23,6 +26,22 @@ export default function Deposit() {
 
           // For batch transactions, the result is an array with the transaction hash as the second element
           return Array.isArray(result) ? result[1].transaction.hash : result
+        }}
+        onEmit={(event) => {
+          if (event.type === "SUCCESSFUL_DEPOSIT") {
+            setNotification({
+              id: crypto.randomUUID(),
+              message: "Deposit successful",
+              type: NotificationType.SUCCESS,
+            })
+          }
+          if (event.type === "FAILED_DEPOSIT") {
+            setNotification({
+              id: crypto.randomUUID(),
+              message: "Deposit failed",
+              type: NotificationType.ERROR,
+            })
+          }
         }}
       />
     </Paper>
