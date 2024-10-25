761d96fec310029df235690d92d40475d4c7a48e
jobotics <jo_botics@proton.me>
2024-10-25 04:22:01 +0100

    feat(deposit): update deposit widget
    - Update token list
    - Refactor sendTransactionNear implementation

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -14,15 +14,15 @@ export default function Deposit() {
     <Paper title="Deposit">
       <DepositWidget
         tokenList={LIST_TOKENS}
-        accountId={state.address || ""}
+        accountId={state.address}
         sendTransactionNear={async (transactions) => {
-          const transactionResult = await sendTransaction({
+          const result = await sendTransaction({
             id: SignInType.NearWalletSelector,
             transactions,
           })
-          // TODO: handle transaction result
-          console.log("transactionResult", transactionResult)
-          return ""
+
+          // For batch transactions, the result is an array with the transaction hash as the second element
+          return Array.isArray(result) ? result[1].transaction.hash : result
         }}
       />
     </Paper>
