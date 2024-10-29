db2c307ee203a5674a71290c5b80124c724cfb52
Old Emil <cawabunga@defuse.org>
2024-10-29 19:13:56 +0100

    fix: do not show toast about swap

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -1,19 +1,15 @@
 "use client"
 
 import { SwapWidget } from "@defuse-protocol/defuse-sdk"
-import { formatUnits } from "viem"
 
 import Paper from "@src/components/Paper"
 import { LIST_TOKENS } from "@src/constants/tokens"
 import { useNearWalletActions } from "@src/hooks/useNearWalletActions"
-import { useNotificationStore } from "@src/providers/NotificationProvider"
 import { useWalletSelector } from "@src/providers/WalletSelectorProvider"
-import { NotificationType } from "@src/stores/notificationStore"
 
 export default function Swap() {
   const { accountId } = useWalletSelector()
   const { signMessage, signAndSendTransactions } = useNearWalletActions()
-  const setNotification = useNotificationStore((state) => state.setNotification)
 
   return (
     <Paper
@@ -45,13 +41,7 @@ export default function Swap() {
 
           return { type: "NEP413", signatureData, signedData }
         }}
-        onSuccessSwap={({ amountOut, tokenOut }) => {
-          setNotification({
-            id: crypto.randomUUID(),
-            message: `Transaction complete! You received ${formatUnits(amountOut, tokenOut.decimals)} ${tokenOut.symbol}`,
-            type: NotificationType.SUCCESS,
-          })
-        }}
+        onSuccessSwap={() => {}}
       />
     </Paper>
   )
