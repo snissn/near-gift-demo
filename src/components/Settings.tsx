387bccca7409ecde97a8af96d2fbf9a8bd410983
jobotics <jo_botics@proton.me>
2024-11-08 10:52:52 +0000

    chore: add feature flag for dark mode

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -13,26 +13,10 @@ import LabelNew from "./LabelNew"
 const NEXT_PUBLIC_LINK_DOCS = process.env.NEXT_PUBLIC_LINK_DOCS ?? ""
 const NEXT_PUBLIC_PUBLIC_MAIL = process?.env?.NEXT_PUBLIC_PUBLIC_MAIL ?? ""
 const NEXT_PUBLIC_PUBLIC_TG = process?.env?.NEXT_PUBLIC_PUBLIC_TG ?? ""
+const DARK_MODE_ENABLED =
+  process?.env?.NEXT_PUBLIC_DARK_MODE === "true" ?? false
 
 const Settings = () => {
-  const { theme, setTheme } = useTheme()
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <reason>
-  useEffect(() => {
-    const getThemeFromLocal = localStorage.getItem(THEME_MODE_KEY)
-    if (!getThemeFromLocal) {
-      setTheme(Themes.LIGHT)
-      return
-    }
-    if (getThemeFromLocal === "light" || getThemeFromLocal === "dark") {
-      setTheme(getThemeFromLocal)
-    }
-  }, [])
-
-  const onChangeTheme = () => {
-    setTheme(theme === Themes.DARK ? Themes.LIGHT : Themes.DARK)
-  }
-
   const elementCircleStyle =
     "bg-black w-[3px] h-[3px] rounded-full dark:bg-gray-100"
   return (
@@ -50,20 +34,7 @@ const Settings = () => {
         </Popover.Trigger>
         <Popover.Content className="min-w-[180px] mt-1 dark:bg-black-800 rounded-2xl">
           <div className="flex flex-col gap-4">
-            <ComingSoon className="top-[0.25rem]">
-              <div className="flex justify-between items-center gap-4">
-                <Text size="2" weight="medium">
-                  Dark Mode
-                </Text>
-                <Switch
-                  className="cursor-pointer"
-                  size="1"
-                  onClick={onChangeTheme}
-                  color="orange"
-                  defaultChecked={theme === Themes.DARK}
-                />
-              </div>
-            </ComingSoon>
+            <DarkMode />
             <Separator orientation="horizontal" size="4" />
             <div className="flex flex-col justify-between items-center gap-1.5">
               <button
@@ -103,6 +74,46 @@ const Settings = () => {
         </Popover.Content>
       </Popover.Root>
     </div>
+  )
+}
+
+const DarkMode = () => {
+  const { theme, setTheme } = useTheme()
+  const onChangeTheme = () => {
+    setTheme(theme === Themes.DARK ? Themes.LIGHT : Themes.DARK)
+  }
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <reason>
+  useEffect(() => {
+    const getThemeFromLocal = localStorage.getItem(THEME_MODE_KEY)
+    if (!getThemeFromLocal) {
+      setTheme(Themes.LIGHT)
+      return
+    }
+    if (getThemeFromLocal === "light" || getThemeFromLocal === "dark") {
+      setTheme(getThemeFromLocal)
+    }
+  }, [])
+
+  const DarkModeSwitch = (
+    <div className="flex justify-between items-center gap-4">
+      <Text size="2" weight="medium">
+        Dark Mode
+      </Text>
+      <Switch
+        className="cursor-pointer"
+        size="1"
+        onClick={onChangeTheme}
+        color="orange"
+        defaultChecked={theme === Themes.DARK}
+      />
+    </div>
+  )
+
+  return DARK_MODE_ENABLED ? (
+    DarkModeSwitch
+  ) : (
+    <ComingSoon className="top-[0.25rem]">{DarkModeSwitch}</ComingSoon>
   )
 }
 
