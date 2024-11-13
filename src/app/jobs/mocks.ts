ef8cffd097e20497ee2b69a49a4537adfc5d47c6
jobotics <jo_botics@proton.me>
2024-11-13 13:56:18 +0000

    chore: open jobs page

--- src/app/jobs/mocks.ts
+++ src/app/jobs/mocks.ts
@@ -4,11 +4,21 @@ export const jobsData: JobsDetails[] = [
   {
     team: "BD",
     position: "VP of Business Development",
-    link: "https://defuse.bamboohr.com/careers/23?source=aWQ9MTg%3D",
+    link: "https://defuse.bamboohr.com/careers/23",
   },
   {
     team: "Marketing",
     position: "Head of Marketing",
-    link: "https://defuse.bamboohr.com/careers/24?source=aWQ9MTg=",
+    link: "https://defuse.bamboohr.com/careers/24",
+  },
+  {
+    team: "Infrastructure",
+    position: "Senior Software Engineer (Rust)",
+    link: "https://defuse.bamboohr.com/careers/25",
+  },
+  {
+    team: "Frontend",
+    position: "Senior Software Engineer (Frontend)",
+    link: "https://defuse.bamboohr.com/careers/26",
   },
 ]
