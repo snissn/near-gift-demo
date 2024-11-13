07f79902013ce0e3fcd99881df7eaa4e7d5e1165
Old Emil <cawabunga@defuse.org>
2024-11-13 09:41:53 +0100

    feat: add "Go to Deposit" link

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,12 +8,14 @@ import { LIST_TOKENS } from "@src/constants/tokens"
 import { ChainType, useConnectWallet } from "@src/hooks/useConnectWallet"
 import { useFlatTokenList } from "@src/hooks/useFlatTokenList"
 import { useNearWalletActions } from "@src/hooks/useNearWalletActions"
+import { useRouter } from "next/navigation"
 
 export default function Swap() {
   const { state } = useConnectWallet()
   const { signMessage, signAndSendTransactions } = useNearWalletActions()
   const { signMessageAsync: signMessageAsyncWagmi } = useSignMessage()
   const tokenList = useFlatTokenList(LIST_TOKENS)
+  const router = useRouter()
 
   return (
     <Paper
@@ -66,6 +68,9 @@ export default function Swap() {
           }
         }}
         onSuccessSwap={() => {}}
+        onNavigateDeposit={() => {
+          router.push("/deposit")
+        }}
       />
     </Paper>
   )
