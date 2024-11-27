854a0263cf5ca85b61a7d9241e6219aabb84500a
Old Emil <cawabunga@defuse.org>
2024-11-27 22:48:34 +0100

    feat: add more Solana wallet adapaters (Phantom, Solflare, WC) (#189)
    * chore: add @solana/wallet-adapter-wallets package
    
    * feat: add Phantom and Solflare wallet adapters
    
    * refactor: move env.SOLANA_RPC_URL
    
    * refactor: reorder imports

--- src/providers/SolanaWalletProvider.tsx
+++ src/providers/SolanaWalletProvider.tsx
@@ -1,17 +1,19 @@
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
+import { type Adapter, WalletAdapterNetwork } from "@solana/wallet-adapter-base"
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react"
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  WalletConnectWalletAdapter,
+} from "@solana/wallet-adapter-wallets"
 import { clusterApiUrl } from "@solana/web3.js"
 import React, { type FC, type ReactNode, useMemo } from "react"
 
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
-
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css"
-
-const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL
 
 export const SolanaWalletProvider: FC<{ children: ReactNode }> = ({
   children,
@@ -20,29 +22,45 @@ export const SolanaWalletProvider: FC<{ children: ReactNode }> = ({
   const network = WalletAdapterNetwork.Mainnet
 
   // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl(network), [network])
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <reason>
-  const wallets = useMemo(
-    () => [
-      /**
-       * Wallets that implement either of these standards will be available automatically.
-       *
-       *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
-       *     (https://github.com/solana-mobile/mobile-wallet-adapter)
-       *   - Solana Wallet Standard
-       *     (https://github.com/anza-xyz/wallet-standard)
-       *
-       * If you wish to support a wallet that supports neither of those standards,
-       * instantiate its legacy wallet adapter here. Common legacy adapters can be found
-       * in the npm package `@solana/wallet-adapter-wallets`.
-       */
-    ],
+  const endpoint = useMemo(
+    () => process.env.SOLANA_RPC_URL || clusterApiUrl(network),
     [network]
   )
 
+  const wallets = useMemo(() => {
+    /**
+     * Wallets that implement either of these standards will be available automatically.
+     *
+     *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+     *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+     *   - Solana Wallet Standard
+     *     (https://github.com/anza-xyz/wallet-standard)
+     *
+     * If you wish to support a wallet that supports neither of those standards,
+     * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+     * in the npm package `@solana/wallet-adapter-wallets`.
+     */
+    const walletAdapters: Adapter[] = [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ]
+
+    if (process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID) {
+      walletAdapters.push(
+        new WalletConnectWalletAdapter({
+          network,
+          options: {
+            projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+          },
+        })
+      )
+    }
+
+    return walletAdapters
+  }, [network])
+
   return (
-    <ConnectionProvider endpoint={SOLANA_RPC_URL ?? endpoint}>
+    <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
