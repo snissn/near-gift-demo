a8ab11860ece561883e6fc6f12fa781f8ea6ea63
depressedPlumber502 <depressed.plumber.502@defuse.org>
2024-11-29 12:41:52 +0400

    feat: fix bg transparency (#192)

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -10,7 +10,7 @@ import ConnectWallet from "@src/components/Wallet"
 const Header = () => {
   return (
     <>
-      <header className="h-[56px] fixed top-0 left-0 w-full md:relative border-b-[1px] border-white-200 z-50 bg-transparent dark:border-black-600">
+      <header className="h-[56px] fixed top-0 left-0 w-full md:relative border-b-[1px] border-white-200 z-50 bg-white dark:bg-black-900 dark:border-black-600">
         <div className="h-full flex justify-between items-center px-3">
           <Logo />
           <div className="hidden md:flex justify-center w-full max-w-5xl pl-3">
