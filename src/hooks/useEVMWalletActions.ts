f7f7ca21b0345160cac97c6c649e73ddc3fb2f80
Old Emil <cawabunga@defuse.org>
2024-12-13 18:30:46 +0100

    fix: switch chain if needed (#239)
    * fix: switch chain if needed
    
    * fix: explicitly specify connector to send transaction through

--- src/hooks/useEVMWalletActions.ts
+++ src/hooks/useEVMWalletActions.ts
@@ -1,33 +1,42 @@
 import type { SendTransactionParameters } from "@wagmi/core"
 import { withTimeout } from "viem"
-import { useSendTransaction, useSwitchChain } from "wagmi"
+import {
+  serialize,
+  useAccount,
+  useSendTransaction,
+  useSwitchChain,
+} from "wagmi"
 
 export function useEVMWalletActions() {
   const { sendTransactionAsync } = useSendTransaction()
   const { switchChainAsync } = useSwitchChain()
+  const { connector } = useAccount()
 
   return {
     sendTransactions: async (tx: SendTransactionParameters) => {
-      console.log("Sending transaction", serializeStructuredLogData({ tx }))
+      console.log("Sending transaction", serialize({ tx }))
 
       const chainId = tx.chainId
-      if (chainId != null) {
-        await withTimeout(() => switchChainAsync({ chainId }), {
+
+      // We can't rely on `chainId` from `useSwitchChain()` or other hooks,
+      // because it might out of sync with the actual chainId of the wallet.
+      const currentChainId = await connector?.getChainId()
+
+      if (chainId != null && currentChainId !== chainId) {
+        console.log("Switching chain", serialize({ currentChainId, chainId }))
+        await withTimeout(() => switchChainAsync({ connector, chainId }), {
           errorInstance: new Error(`Chain switch timeout chainId=${chainId}`),
           // WalletConnect issue: when network switching is not possible, it'll hang forever, so we need to set a timeout
           timeout: 30000,
         })
       }
 
-      const txHash = await sendTransactionAsync(tx)
+      const txHash = await sendTransactionAsync({
+        connector,
+        ...tx,
+      })
 
       return txHash
     },
   }
-}
-
-function serializeStructuredLogData(data: object) {
-  return JSON.stringify(data, (key, value) =>
-    typeof value === "bigint" ? value.toString() : value
-  )
 }
