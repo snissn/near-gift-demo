43faa65309812fced096b93a628df6a0ef0180c9
Old Emil <cawabunga@defuse.org>
2025-02-17 18:54:19 +0100

    feat: add webauthn API routes (#303)
    * chore: add msw package
    
    * chore: run msw in tests
    
    * feat: add api routes for webauthn credentials (create and read)
    
    * feat: add FE functions for calling webauthn_credentials api

---
+++ src/api/webauthnCredentials.ts
@@ -0,0 +1,44 @@
+import type {
+  CreateCredentialResponse,
+  ErrorResponse,
+  GetCredentialResponse,
+  WebauthnCredential,
+} from "@src/types/api/webauthnCredentials"
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? ""
+
+export async function createWebauthnCredential(credential: WebauthnCredential) {
+  const response = await fetch(`${BASE_URL}/api/webauthn_credentials`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credential),
+  })
+
+  if (!response.ok) {
+    const error = (await response.json()) as ErrorResponse
+    throw new Error(
+      typeof error.error === "string"
+        ? error.error
+        : "Failed to create credential"
+    )
+  }
+
+  return response.json() as Promise<CreateCredentialResponse>
+}
+
+export async function getWebauthnCredential(rawId: string) {
+  const response = await fetch(`${BASE_URL}/api/webauthn_credentials/${rawId}`)
+
+  if (!response.ok) {
+    const error = (await response.json()) as ErrorResponse
+    throw new Error(
+      typeof error.error === "string"
+        ? error.error
+        : "Failed to fetch credential"
+    )
+  }
+
+  return response.json() as Promise<GetCredentialResponse>
+}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  resolve: {
+  test: {
+    setupFiles: ["./src/tests/setup.ts"],
     alias: {
       "@src": "/src",
     },
