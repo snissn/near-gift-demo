24bf504fba96577bf8e3a5207b4c6098676acd48
Old Emil <cawabunga@defuse.org>
2025-03-03 18:31:45 +0100

    fix: remove Nightly wallet support (#328)
    * chore: remove @near-wallet-selector/nightly package
    
    * fix: remove nightly wallet integration

--- src/providers/WalletSelectorProvider.tsx
+++ src/providers/WalletSelectorProvider.tsx
@@ -7,7 +7,6 @@ import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet"
 import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui"
 import { setupModal } from "@near-wallet-selector/modal-ui"
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet"
-import { setupNightly as setupNightlyWallet } from "@near-wallet-selector/nightly"
 import {
   type ReactNode,
   createContext,
@@ -62,12 +61,7 @@ export const WalletSelectorProvider: React.FC<{
           "postgres://public_readonly:nearprotocol@mainnet.db.explorer.indexer.near.dev/mainnet_explorer",
       },
       debug: true,
-      modules: [
-        setupMyNearWallet(),
-        setupMeteorWallet(),
-        setupNightlyWallet(),
-        setupHotWallet(),
-      ],
+      modules: [setupMyNearWallet(), setupMeteorWallet(), setupHotWallet()],
     })
     const _modal = setupModal(_selector, {
       contractId: "",
