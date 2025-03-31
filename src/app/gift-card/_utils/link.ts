4f2a122bb731d09f827ae1ed76cee6f87f431033
jobotics <jo_botics@proton.me>
2025-03-31 13:26:59 +0100

    fix: hash fragment (#359)

--- src/app/gift-card/_utils/link.ts
+++ src/app/gift-card/_utils/link.ts
@@ -5,15 +5,14 @@ import { decodeGift, encodeGift } from "./encoder"
 
 export function createGiftCardLink(payload: unknown): string {
   const url = new URL("/gift-card/view-gift", window.location.origin)
-  url.searchParams.set("#", encodeGift(payload))
+  url.hash = encodeGift(payload)
   return url.toString()
 }
 
 export function useGiftCard() {
-  const encodedGift = useSearchParams().get("#")
-
   const [payload] = useState(() => {
     try {
+      const encodedGift = window.location.hash.slice(1)
       return decodeGift(encodedGift ?? "")
     } catch (e) {
       logger.error(e)
