41b823b8e5f41bf0f3dc466d4ac0dd76f662730d
Old Emil <cawabunga@defuse.org>
2025-04-16 20:13:02 +0200

    feat: add liquidity alert processing to Sentry error events (#387)

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,7 +2,10 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import { isBaseToken } from "@defuse-protocol/defuse-sdk"
 import * as Sentry from "@sentry/nextjs"
+import { LIST_TOKENS } from "@src/constants/tokens"
+import * as v from "valibot"
 
 Sentry.init({
   dsn: "https://12f8f38e9e78e2900f386bec2549c9d7@o4504157766942720.ingest.us.sentry.io/4507589484544000",
@@ -18,4 +21,52 @@ Sentry.init({
       blockAllMedia: false,
     }),
   ],
+  beforeSend: (event) => {
+    return processNoLiquidityEvent(event)
+  },
+})
+
+function processNoLiquidityEvent(event: Sentry.ErrorEvent) {
+  if (!v.is(noLiquidityEventSchema, event)) {
+    return event
+  }
+
+  const tokenIn = toToken(event.contexts.quoteParams.defuse_asset_identifier_in)
+  const tokenOut = toToken(
+    event.contexts.quoteParams.defuse_asset_identifier_out
+  )
+
+  const event_: Sentry.ErrorEvent = event
+  event_.tags ??= {}
+  event_.tags["liquidity-alerts"] = true
+  event_.message = `No liquidity available for $${tokenIn?.symbol} (${tokenIn?.chainName}) to $${tokenOut?.symbol} (${tokenOut?.chainName})`
+  return event_
+}
+
+function toToken(defuseAssetId: string) {
+  for (const token of LIST_TOKENS) {
+    if (isBaseToken(token)) {
+      if (token.defuseAssetId === defuseAssetId) {
+        return token
+      }
+    } else {
+      for (const t of token.groupedTokens) {
+        if (t.defuseAssetId === defuseAssetId) {
+          return t
+        }
+      }
+    }
+  }
+}
+
+const noLiquidityEventSchema = v.object({
+  message: v.literal(
+    "quote: No liquidity available for user with sufficient balance"
+  ),
+  contexts: v.object({
+    quoteParams: v.object({
+      defuse_asset_identifier_in: v.string(),
+      defuse_asset_identifier_out: v.string(),
+    }),
+  }),
 })
