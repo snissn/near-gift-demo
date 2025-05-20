2cb4d65a0fd59d4f3d1efbeecf4d2e4ae2d542b4
jobotics <jo_botics@proton.me>
2025-05-20 11:16:12 +0100

    chore: minify gift json and reduce 22 chars (#418)

--- src/app/gift-card/_utils/encoder.ts
+++ src/app/gift-card/_utils/encoder.ts
@@ -1,14 +1,11 @@
 import { base64urlnopad } from "@scure/base"
 
 export function encodeGift(gift: unknown): string {
-  const format = {
-    version: 1,
-    payload: gift,
-  }
-  return base64urlnopad.encode(new TextEncoder().encode(JSON.stringify(format)))
+  return base64urlnopad.encode(new TextEncoder().encode(JSON.stringify(gift)))
 }
 
 export function decodeGift(encodedGift: string): string {
   const json = new TextDecoder().decode(base64urlnopad.decode(encodedGift))
-  return JSON.parse(json).payload
+  const parsed = JSON.parse(json)
+  return parsed.payload ?? parsed // check payload for backwards compatibility
 }
