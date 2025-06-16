2645c1131282d373d7f8452ad70d63376944f75b
depressedPlumber502 <depressed.plumber.502@defuse.org>
2025-06-16 18:52:45 +0400

    feat: use Link for navigation in desktop (#460)

--- src/components/Navbar/NavbarDesktop.tsx
+++ src/components/Navbar/NavbarDesktop.tsx
@@ -5,11 +5,10 @@ import { Button } from "@radix-ui/themes"
 import { navigation } from "@src/constants/routes"
 import { useIsActiveLink } from "@src/hooks/useIsActiveLink"
 import { cn } from "@src/utils/cn"
-import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 export function NavbarDesktop() {
   const { isActive } = useIsActiveLink()
-  const router = useRouter()
 
   const isAccountActive = isActive(navigation.account)
   const isTradeActive = isActive(navigation.home) || isActive(navigation.otc)
@@ -20,15 +19,11 @@ export function NavbarDesktop() {
       <NavItem
         label="Account"
         isActive={isAccountActive}
-        onNavigate={() => router.push(navigation.account)}
+        href={navigation.account}
       />
 
       {/* Trade */}
-      <NavItem
-        label="Trade"
-        isActive={isTradeActive}
-        onNavigate={() => router.push(navigation.home)}
-      />
+      <NavItem label="Trade" isActive={isTradeActive} href={navigation.home} />
     </nav>
   )
 }
@@ -36,43 +31,44 @@ export function NavbarDesktop() {
 function NavItem({
   label,
   isActive,
-  onNavigate,
+  href,
 }: {
   label: string
   isActive: boolean
-  onNavigate: () => void
+  href: string
 }) {
   return (
-    <Button
-      radius="full"
-      color="gray"
-      highContrast
-      variant={isActive ? "solid" : "soft"}
-      className={cn(
-        "relative text-sm cursor-pointer",
-        isActive ? "text-gray-1" : "bg-transparent"
-      )}
-      onClick={onNavigate}
-      asChild
-    >
-      <span className="text-sm font-bold whitespace-nowrap">{label}</span>
-    </Button>
+    <Link href={href}>
+      <Button
+        radius="full"
+        color="gray"
+        highContrast
+        variant={isActive ? "solid" : "soft"}
+        className={cn(
+          "relative text-sm cursor-pointer",
+          isActive ? "text-gray-1" : "bg-transparent"
+        )}
+        asChild
+      >
+        <span className="text-sm font-bold whitespace-nowrap">{label}</span>
+      </Button>
+    </Link>
   )
 }
 
 export function NavbarDeposit() {
-  const router = useRouter()
   return (
-    <Button
-      radius="full"
-      color="gray"
-      highContrast
-      variant="soft"
-      className="flex items-center gap-2 text-sm cursor-pointer"
-      onClick={() => router.push(navigation.deposit)}
-    >
-      <Plus className="size-3 text-gray-12" weight="bold" />
-      <span className="text-sm font-bold whitespace-nowrap">Deposit</span>
-    </Button>
+    <Link href={navigation.deposit}>
+      <Button
+        radius="full"
+        color="gray"
+        highContrast
+        variant="soft"
+        className="flex items-center gap-2 text-sm cursor-pointer"
+      >
+        <Plus className="size-3 text-gray-12" weight="bold" />
+        <span className="text-sm font-bold whitespace-nowrap">Deposit</span>
+      </Button>
+    </Link>
   )
 }
