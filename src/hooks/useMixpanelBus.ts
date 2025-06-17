9ce72d51384429d8fc685ccdd44e291b6985afb5
dr-block <artur.movsisyan@defuse.org>
2025-06-17 17:29:40 +0400

    fix: mixpanel events - remove unnecessary logger (#462)
    Co-authored-by: dr-block <dr-block@defuse.org>

--- src/hooks/useMixpanelBus.ts
+++ src/hooks/useMixpanelBus.ts
@@ -28,7 +28,6 @@ export function useMixpanelBus() {
 
   const sendMixPanelEvent = useCallback(
     (eventName: string, payload: Dict) => {
-      console.log("mixPanel", mixPanel)
       mixPanel?.track(eventName, JSON.parse(serialize(payload)))
     },
     [mixPanel]
