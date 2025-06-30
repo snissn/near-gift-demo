0e0882790370dea1623f5617692cd7f5049ba801
jobotics <jo_botics@proton.me>
2025-06-30 12:27:07 +0100

    chore: temporary disable TON and WBTC (#480)

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1361,29 +1361,31 @@ export const PRODUCTION_TOKENS: TokenWithTags[] = [
     name: "Kyber Network Crystal v2",
     tags: ["mc:461"],
   },
-  {
-    defuseAssetId: "nep245:v2_1.omni.hot.tg:1117_",
-    type: "native",
-    decimals: 9,
-    icon: "https://s2.coinmarketcap.com/static/img/coins/128x128/11419.png",
-    chainName: "ton",
-    bridge: "hot_omni",
-    symbol: "TON",
-    name: "TON",
-    tags: ["mc:18"],
-  },
-  {
-    defuseAssetId:
-      "nep141:eth-0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599.omft.near",
-    address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
-    decimals: 8,
-    icon: "https://s2.coinmarketcap.com/static/img/coins/128x128/3717.png",
-    chainName: "eth",
-    bridge: "poa",
-    symbol: "WBTC",
-    name: "Wrapped Bitcoin",
-    tags: ["mc:3"],
-  },
+  // Temporarily disable TON until HOT fixes passive withdrawals
+  // {
+  //   defuseAssetId: "nep245:v2_1.omni.hot.tg:1117_",
+  //   type: "native",
+  //   decimals: 9,
+  //   icon: "https://s2.coinmarketcap.com/static/img/coins/128x128/11419.png",
+  //   chainName: "ton",
+  //   bridge: "hot_omni",
+  //   symbol: "TON",
+  //   name: "TON",
+  //   tags: ["mc:18"],
+  // },
+  // Temporary disable WBTC until bridge is ready
+  // {
+  //   defuseAssetId:
+  //     "nep141:eth-0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599.omft.near",
+  //   address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+  //   decimals: 8,
+  //   icon: "https://s2.coinmarketcap.com/static/img/coins/128x128/3717.png",
+  //   chainName: "eth",
+  //   bridge: "poa",
+  //   symbol: "WBTC",
+  //   name: "Wrapped Bitcoin",
+  //   tags: ["mc:3"],
+  // },
 ]
 
 const STAGE_TOKENS: TokenWithTags[] = [
