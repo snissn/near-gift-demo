8a2814a28be62cfc18270afe6706a278f26e9afa
jobotics <jo_botics@proton.me>
2025-07-24 13:25:07 +0100

    chore: remove CoinEasy and add EasyChain icon (#510)
    * chore:  add icon for EasyChain
    
    * chore: remove unused CoinEasy icon
    
    * chore: bump @defuse-protocol/defuse-sdk
    
    * chore: rename chainName coineasy to easychain in tokens
    
    * fix: correct logger context in configureSDK

--- src/libs/defuse-sdk/initSDK.ts
+++ src/libs/defuse-sdk/initSDK.ts
@@ -1,5 +1,6 @@
 import { configureSDK } from "@defuse-protocol/defuse-sdk/config"
 import * as Sentry from "@sentry/core"
+import type { Context, Contexts } from "@sentry/types"
 import { INTENTS_ENV, NODE_IS_DEVELOPMENT } from "@src/utils/environment"
 
 let hasInitialized = false
@@ -24,6 +25,7 @@ export function initSDK() {
       env: INTENTS_ENV,
       logger: {
         verbose: console.log,
+        debug: console.info,
         info: console.info,
         warn: console.warn,
         error: console.error,
@@ -48,14 +50,26 @@ export function initSDK() {
             data: data,
           })
         },
+        debug: (msg, contexts) => {
+          Sentry.captureMessage(msg, {
+            contexts: sanitizeContexts(contexts),
+            level: "debug",
+          })
+        },
         info: (msg, contexts) => {
-          Sentry.captureMessage(msg, { contexts, level: "info" })
+          Sentry.captureMessage(msg, {
+            contexts: sanitizeContexts(contexts),
+            level: "info",
+          })
         },
         warn: (msg, contexts) => {
-          Sentry.captureMessage(msg, { contexts, level: "warning" })
+          Sentry.captureMessage(msg, {
+            contexts: sanitizeContexts(contexts),
+            level: "warning",
+          })
         },
         error: (err, contexts) => {
-          Sentry.captureException(err, { contexts })
+          Sentry.captureException(err, { contexts: sanitizeContexts(contexts) })
         },
       },
       features: {
@@ -68,4 +82,23 @@ export function initSDK() {
       },
     })
   }
+}
+
+function isContext(value: unknown): value is Context {
+  return typeof value === "object" && value !== null && !Array.isArray(value)
+}
+
+// Utility to sanitize contexts for Sentry
+function sanitizeContexts(
+  contexts: Record<string, unknown> | undefined
+): Contexts | undefined {
+  if (!contexts) return undefined
+  const sanitized: Contexts = {}
+  for (const key in contexts) {
+    const value = contexts[key]
+    if (isContext(value)) {
+      sanitized[key] = value
+    }
+  }
+  return Object.keys(sanitized).length > 0 ? sanitized : undefined
 }
