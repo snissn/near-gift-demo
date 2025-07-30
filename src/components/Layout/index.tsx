6003a8965fb8e19c0f610d3988451d136fbeb7b9
Old Emil <cawabunga@defuse.org>
2025-07-30 21:02:55 +0200

    Revert "feat: add Polygon outage notification component (#522)"
    This reverts commit 8a18839d617e3d47f11d667cb1d73e125ff01cba.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,7 +7,6 @@ import Footer from "@src/components/Layout/Footer"
 import { Header } from "@src/components/Layout/Header"
 import { NavbarMobile } from "@src/components/Navbar/NavbarMobile"
 import PageBackground from "@src/components/PageBackground"
-import PolygonOutageNotification from "@src/components/PolygonOutageNotification"
 import { useMixpanelBus } from "@src/hooks/useMixpanelBus"
 import { usePathLogging } from "@src/hooks/usePathLogging"
 import { WalletVerificationProvider } from "@src/providers/WalletVerificationProvider"
@@ -34,7 +33,6 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
           </Header.DepositSlot>
         }
       />
-      <PolygonOutageNotification />
       <Main>{children}</Main>
       <Footer />
       <NavbarMobile />
